a125e7663662d45e791ad8f0d9b1ece4d3ac4133
Marcus Cemes <marcus.cemes@gmail.com>
2023-12-04 10:20:32 +0100

    feat: day 04

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Hi! These are my Rust solutions for the [Advent of Code 2023][advent-of-code].
 | [1][p01] | Trebuchet?!    | [01.rs][s01] |   ⭐   |   ⭐   | 33.1 µs | 42.1 µs |
 | [2][p02] | Cube Conundrum | [02.rs][s02] |   ⭐   |   ⭐   | 26.5 µs | 37.2 µs |
 | [3][p03] | Gear Ratios    | [03.rs][s03] |   ⭐   |   ⭐   | 39.1 µs | 30.7 µs |
+| [4][p04] | Scratchcards   | [04.rs][s04] |   ⭐   |   ⭐   | 82.4 µs | 84.0 µs |
 |          | ...            |              |        |        |         |         |
 
 **Key**: ⭐ Completed &nbsp;&nbsp; 🎁 In progress &nbsp;&nbsp; 😔 Gave up
@@ -28,13 +29,15 @@ This repository uses a modified version of [this template][template]. Thanks Fel
 Distributed under the MIT Licence. See [LICENCE](LICENCE) for more information.
 
 [rust-badge]: https://img.shields.io/badge/Rust-d55826?logo=rust&style=for-the-badge
-[solutions-badge]: https://img.shields.io/badge/solutions-6/50-brightgreen?logo=star&style=for-the-badge
+[solutions-badge]: https://img.shields.io/badge/solutions-8/50-brightgreen?logo=star&style=for-the-badge
 [advent-of-code]: https://adventofcode.com/
 [rust]: https://www.rust-lang.org/
 [template]: https://github.com/fspoettel/advent-of-code-rust
 [p01]: https://adventofcode.com/2023/day/1
 [p02]: https://adventofcode.com/2023/day/2
 [p03]: https://adventofcode.com/2023/day/3
+[p04]: https://adventofcode.com/2023/day/4
 [s01]: src/bin/01.rs
 [s02]: src/bin/02.rs
 [s03]: src/bin/03.rs
+[s04]: src/bin/04.rs
---
+++ src/bin/04.rs
@@ -0,0 +1,74 @@
+advent_of_code::solution!(4);
+
+const SIZE_HINT: usize = 200;
+
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(
+        input
+            .lines()
+            .map(|line| count_score(winning_count(line)))
+            .sum(),
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut extra_counts: Vec<u32> = vec![0; SIZE_HINT];
+    let mut count = 0;
+
+    for (i, line) in input.lines().enumerate() {
+        let instances = extra_counts.get(i).unwrap_or(&0) + 1;
+        let winners = winning_count(line);
+
+        // Removed dynamic resizing in favour of fixed SIZE_HINT
+        // let new_size = extra_counts.len().max(i + winners) + 1;
+        // extra_counts.resize(new_size, 0);
+
+        for j in 1..=winners {
+            extra_counts[i + j] += instances;
+        }
+
+        count += instances;
+    }
+
+    Some(count)
+}
+
+fn winning_count(line: &str) -> usize {
+    let (_, rest) = line.split_once(": ").unwrap();
+    let (target, cards) = rest.split_once(" | ").unwrap();
+
+    let targets: Vec<u8> = target
+        .split_ascii_whitespace()
+        .map(|n| n.parse().unwrap())
+        .collect();
+
+    cards
+        .split_ascii_whitespace()
+        .map(|n| n.parse().unwrap())
+        .filter(|n| targets.contains(n))
+        .count()
+}
+
+fn count_score(count: usize) -> u32 {
+    match count {
+        0 => 0,
+        n => 1 << (n - 1),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(13));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(30));
+    }
+}
