dc1f68e10721f2a0e290efe5c051be4bba312879
Marcus Cemes <marcus.cemes@gmail.com>
2023-12-06 12:56:03 +0100

    feat: day 06

--- README.md
+++ README.md
@@ -6,13 +6,14 @@ Hi! These are my Rust solutions for the [Advent of Code 2023][advent-of-code].
 
 <div align="center">
 
-|      Day | Name                            | Source       | Part 1 | Part 2 | Time 1  | Time 2  |
-| -------: | ------------------------------- | ------------ | :----: | :----: | :-----: | :-----: |
+|      Day | Name                            | Source       | Part 1 | Part 2 |  Time 1 |  Time 2 |
+| -------: | ------------------------------- | ------------ | :----: | :----: | ------: | ------: |
 | [1][p01] | Trebuchet?!                     | [01.rs][s01] |   ⭐   |   ⭐   | 33.1 µs | 42.1 µs |
 | [2][p02] | Cube Conundrum                  | [02.rs][s02] |   ⭐   |   ⭐   | 26.5 µs | 37.2 µs |
 | [3][p03] | Gear Ratios                     | [03.rs][s03] |   ⭐   |   ⭐   | 39.1 µs | 30.7 µs |
 | [4][p04] | Scratchcards                    | [04.rs][s04] |   ⭐   |   ⭐   | 82.4 µs | 84.0 µs |
 | [5][p05] | If You Give A Seed A Fertilizer | [05.rs][s05] |   ⭐   |   ⭐   | 18.7 µs | 23.5 µs |
+| [6][p06] | Wait For It                     | [06.rs][s06] |   ⭐   |   ⭐   |  174 ns |  180 ns |
 |          | ...                             |              |        |        |         |         |
 
 **Key**: ⭐ Completed &nbsp;&nbsp; 🎁 In progress &nbsp;&nbsp; 😔 Gave up
@@ -30,7 +31,7 @@ This repository uses a modified version of [this template][template]. Thanks Fel
 Distributed under the MIT Licence. See [LICENCE](LICENCE) for more information.
 
 [rust-badge]: https://img.shields.io/badge/Rust-d55826?logo=rust&style=for-the-badge
-[solutions-badge]: https://img.shields.io/badge/solutions-10/50-brightgreen?logo=star&style=for-the-badge
+[solutions-badge]: https://img.shields.io/badge/solutions-12/50-brightgreen?logo=star&style=for-the-badge
 [advent-of-code]: https://adventofcode.com/
 [rust]: https://www.rust-lang.org/
 [template]: https://github.com/fspoettel/advent-of-code-rust
@@ -39,8 +40,10 @@ Distributed under the MIT Licence. See [LICENCE](LICENCE) for more information.
 [p03]: https://adventofcode.com/2023/day/3
 [p04]: https://adventofcode.com/2023/day/4
 [p05]: https://adventofcode.com/2023/day/5
+[p06]: https://adventofcode.com/2023/day/6
 [s01]: src/bin/01.rs
 [s02]: src/bin/02.rs
 [s03]: src/bin/03.rs
 [s04]: src/bin/04.rs
 [s05]: src/bin/05.rs
+[s06]: src/bin/06.rs
---
+++ src/bin/06.rs
@@ -0,0 +1,112 @@
+advent_of_code::solution!(6);
+
+/* == Definitions == */
+
+struct Race {
+    distance: u64,
+    time: u64,
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let solution = parse_separate(input)
+        .map(|game| number_winning_possibilities(&game))
+        .product();
+
+    Some(solution)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(number_winning_possibilities(&parse_joined(input)))
+}
+
+/* == Parsing == */
+
+fn parse_separate(input: &str) -> impl Iterator<Item = Race> + '_ {
+    let (distance_str, time_str) = parse_input(input);
+
+    iter_numbers(distance_str)
+        .zip(iter_numbers(time_str))
+        .map(|(distance, time)| Race { time, distance })
+}
+
+fn parse_joined(input: &str) -> Race {
+    let (distance_str, time_str) = parse_input(input);
+    let time = joined_numbers(time_str);
+    let distance = joined_numbers(distance_str);
+    Race { time, distance }
+}
+
+fn parse_input(input: &str) -> (&str, &str) {
+    let mut lines = input.lines().map(|l| l.split_at(9).1);
+    let time_str = lines.next().unwrap();
+    let distance_str = lines.next().unwrap();
+    (distance_str, time_str)
+}
+
+fn iter_numbers(input: &str) -> impl Iterator<Item = u64> + '_ {
+    input.split_whitespace().map(|s| s.parse().unwrap())
+}
+
+fn joined_numbers(input: &str) -> u64 {
+    input
+        .split_whitespace()
+        .map(|s| (s.len() as u32, s.parse::<u64>().unwrap()))
+        .fold(0, |acc, (s, n)| 10_u64.pow(s) * acc + n)
+}
+
+/* == Functions == */
+
+/// Grug tried counting the number of winning possibilities by hand,
+/// but he got bored after a while. He decided to write a program
+/// to do it for him, but he didn't know how to solve quadratic equations.
+/// So he asked his friend, who was a mathematician, to help him.
+/// His friend told him that the number of winning possibilities is
+/// given by the number of natural numbers x that solve the inequation
+///  x * (t - x) - d > 0, where x is the time pressing the button, t is the time
+/// of the race and d is the record distance. Grug was happy with this answer,
+/// and he wrote a program to solve it. Computer didn't get bored, unlike Grug.
+/// Grug was able to win the race.
+fn number_winning_possibilities(race: &Race) -> u32 {
+    let a = -1.0;
+    let b = race.time as f64;
+    let c = -(race.distance as f64);
+
+    let (x1, x2) = solutions_of_quadratic_equation(a, b, c);
+
+    let mut diff = x2.floor() - x1.ceil() - 1.0;
+    diff += if x1.trunc() != x1 { 1.0 } else { 0.0 };
+    diff += if x2.trunc() != x2 { 1.0 } else { 0.0 };
+
+    diff as u32
+}
+
+/// Computes the solutions of the quadratic equation ax^2 + bx + c = 0.
+/// The computation is done using f64, as f32 was not sufficient for the input.
+fn solutions_of_quadratic_equation(a: f64, b: f64, c: f64) -> (f64, f64) {
+    let discriminant = b * b - 4.0 * a * c;
+    debug_assert!(discriminant > 0.0);
+    let sqrt_discriminant = discriminant.sqrt();
+    let two_a = 2.0 * a;
+    let x1 = (-b + sqrt_discriminant) / two_a;
+    let x2 = (-b - sqrt_discriminant) / two_a;
+    (x1, x2)
+}
+
+/* == Tests == */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(288));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(71503));
+    }
+}
